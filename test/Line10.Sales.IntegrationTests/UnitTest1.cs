eccc359a4f96082c5aae7c9b56edd041fe176bd0
cpilao <carlospilao@gmail.com>
2025-02-10 09:50:19 +0000

    chore: init solution projects

---
+++ test/Line10.Sales.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Line10.Sales.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ test/Line10.Sales.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Line10.Sales.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
